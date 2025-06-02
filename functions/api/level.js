6729c3def524c36037b81fd46021d1822f3d5d62
Longyearniue <taiichifox@gmail.com>
2025-06-03 04:50:34 +0900

    Create level.js

---
+++ functions/api/level.js
@@ -0,0 +1,60 @@
+export async function onRequest(context) {
+  const { request, env } = context;
+  
+  if (request.method === 'OPTIONS') {
+    return new Response(null, {
+      status: 204,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      },
+    });
+  }
+
+  if (request.method !== 'POST') {
+    return new Response('Method not allowed', { 
+      status: 405,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      }
+    });
+  }
+
+  try {
+    const { currentLevel, currentXP } = await request.json();
+    const newXP = currentXP + 1;
+    
+    const levelThresholds = [0, 1, 5, 10, 20, 50, 100, 200, 500, 1000];
+    let newLevel = currentLevel;
+    
+    for (let i = levelThresholds.length - 1; i >= 0; i--) {
+      if (newXP >= levelThresholds[i]) {
+        newLevel = i + 1;
+        break;
+      }
+    }
+    
+    return new Response(JSON.stringify({
+      level: newLevel,
+      xp: newXP,
+      leveledUp: newLevel > currentLevel
+    }), {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+    });
+  } catch (error) {
+    console.error('Level API error:', error);
+    return new Response(JSON.stringify({
+      error: 'Internal server error'
+    }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+    });
+  }
+}
